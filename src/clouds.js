8c903efc0371077f2d8988b885b36eff9bf9b891
David Purnell <dave@purnell.io>
2023-11-19 11:37:49 -0500

    new approach to full height on iphone, adding missing poem line

--- src/clouds.js
+++ src/clouds.js
@@ -2,19 +2,45 @@
 import "./scss/clouds.scss";
 import "animate.css";
 
+const vw = Math.max(
+  document.documentElement.clientWidth || 0,
+  window.innerWidth || 0
+);
+const vh = Math.max(
+  document.documentElement.clientHeight || 0,
+  window.innerHeight || 0
+);
+
+function reflow(element) {
+  if (element === undefined) {
+    element = document.documentElement;
+  }
+  void element.offsetHeight;
+}
+// console.log(screen.orientation);
+window.addEventListener("orientationchange", () => {
+  // reflow();
+  location.reload();
+});
+
+
 const sleep = (ms) => {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 const allStanzas = document.getElementById("all");
 const firstStanza = document.getElementById("first");
+const footer = document.getElementsByTagName("footer")[0];
 
 const morePoetry = async () => {
   firstStanza.classList.remove('animate__fadeIn');
   firstStanza.classList.add('animate__fadeOut');
+  footer.classList.add('animate__fadeOut');
   await sleep(300);
   firstStanza.classList.remove('shown','animate__fadeOut');
+  footer.classList.remove('shown','animate__fadeOut');
   allStanzas.classList.add('animate__fadeIn','shown');
+  // reflow();
 }
 window.morePoetry = morePoetry;
 
@@ -24,5 +50,6 @@ const lessPoetry = async () => {
   await sleep(300);
   allStanzas.classList.remove('shown','animate__fadeOut');
   firstStanza.classList.add('animate__fadeIn','shown');
+  footer.classList.add('animate__fadeIn','shown');
 }
 window.lessPoetry = lessPoetry;
