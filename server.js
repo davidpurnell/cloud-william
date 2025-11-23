d8abf53a8dd6fb16314e80450fd1074ab61c3b30
Dave <dave@maxmin.local>
2025-11-23 15:40:33 -0500

    adding an express server to get this working on Xcloud managed servers

--- server.js
+++ server.js
@@ -4,12 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from dist directory
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Handle client-side routing - serve index.html for all routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+app.use(express.static(path.join(__dirname, 'dist'), {
+  extensions: ['html']
+}));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
